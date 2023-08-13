935edfbccd6f047aea61d7af04c47eff61799ff4
leehoisleeho <leehovip@gmail.com>
2023-08-13 21:42:13 +0800

    订单详情页没完成

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
 // app.js
-
 App({
-  globalData: {
-    isLoading: false // 设置一个全局变量，用于标识是否正在加载数据
-  },
-  onLaunch() {
-   
-  },
+  onLaunch() {},
 });
--- pages/order/order.js
+++ pages/order/order.js
@@ -1,14 +1,23 @@
 import api  from "../../API/api";
-const app = getApp()
+
 Page({
-  onTabsChange(event) {
-    console.log(`Change tab, tab-panel value is ${event.detail.value}.`);
-  },
-
+  //点击
   onTabsClick(event) {
-    console.log(`Click tab, tab-panel value is ${event.detail.value}.`);
+    let index = Number(event.detail.value)
+    if(index===0){
+      // 全部
+      this.getPageDate()
+    }else if(index===1){
+      // 上门
+      this.getPageDate("0")
+    }else if(index===2){
+      // 完成
+      
+    }else if(index===3){
+      // 取消
+      
+    }
   },
-
   toDetails() {
     wx.navigateTo({
       url: "../orderDetails/orderDetails",
@@ -17,28 +26,36 @@ Page({
   data:{
     orderList : [],
     isEmpty:null,
-    isShow:false
+    isShow:false,
+  },
+  // 补零函数
+  padZero(num) {
+    return num < 10 ? '0' + num : num;
+  },
+  // 时间戳转换
+  formatTimestamp(timestamp) {
+    const date = new Date(timestamp * 1000); // 转换为毫秒
+    const year = date.getFullYear();
+    const month = this.padZero(date.getMonth() + 1); // 月份从0开始，所以需要+1
+    const day = this.padZero(date.getDate());
+    const hours = this.padZero(date.getHours());
+    const minutes = this.padZero(date.getMinutes());
+    const seconds = this.padZero(date.getSeconds());
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
   },
   onShow() {
     this.setData({
       isShow:false
     })
-    if (app.globalData.isLoading) {
-      wx.showToast({
-        title: '请等待数据加载完成',
-        icon: 'none'
-      });
-      // 阻止切换 Tab
-      wx.switchTab({
-        url: '/pages/当前Tab页面的路径'
-      });
-    }
     this.getPageDate()
   },
   // 获取页面数据
   async getPageDate(){
-    let orderList = await api.getOrderList()
+    let orderList = await api.getOrderList({
+      
+    })
     orderList = orderList.data.data.data
+    console.log(orderList)
     if(orderList.length == 0 ){
       this.setData({
         orderList,
@@ -46,6 +63,10 @@ Page({
         isShow:true
       })
     }else{
+      orderList.map(item=>{
+        const formattedTime = this.formatTimestamp(item.createtime)
+        item.createtime = formattedTime
+      })
       this.setData({
         orderList,
         isEmpty:false,
